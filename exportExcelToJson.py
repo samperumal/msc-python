fffc670cbedf2c53891b9aafea6432746a94007d
Sam Perumal <sam_lki@yahoo.com>
2019-04-26 15:54:29 +0200

    Export dimensions form xlsx to json

---
+++ exportExcelToJson.py
@@ -0,0 +1,26 @@
+from openpyxl import load_workbook;
+import os;
+import json;
+
+def main(headersAddress, dataAddress):
+    wb = load_workbook("jsroot/TRDDimensions.xlsx")
+    ws = wb.active
+    headers = [header.value for row in ws[headersAddress] for header in row ]
+    print(headers)
+    nestedData = [list(zip(headers, [d.value for d in row])) for row in ws[dataAddress]]
+    data = []
+    for row in nestedData:
+        obj = {}
+        for pair in row:
+            obj[pair[0]] = pair[1]
+        data.append(obj)
+
+    outfile = open("jsroot/TrdDimensions.js", "w")
+    print("function getDimensions() { return ", file = outfile, end = '')
+    json.dump(data, outfile, indent = 4)
+    print(";\n}", file = outfile)
+
+    outfile.close()
+
+if __name__ == "__main__":
+    main("A3:M3", "A4:M33")
