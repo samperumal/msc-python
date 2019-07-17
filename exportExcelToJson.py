dae7a0dae7f77eccceb05db1b1c1c90f6d37da03
Sam Perumal <sam_lki@yahoo.com>
2019-07-17 12:25:46 +0200

    Output padrow individual dimensions

--- exportExcelToJson.py
+++ exportExcelToJson.py
@@ -9,6 +9,7 @@ def main(headersAddress, dataAddress):
     
     nestedData = [list(zip(headers, [d.value for d in row])) for row in ws[dataAddress]]
     data = []
+    padrows = []
     for row in nestedData:
         obj = {}
         for pair in row:
@@ -16,10 +17,38 @@ def main(headersAddress, dataAddress):
         data.append(obj)
 
     outfile = open("jsroot/components/trd-dimensions.js", "w")
-    print("function getDimensions() { return ", file = outfile, end = '')
+    print("function getDimensions() { const dims = ", file = outfile, end = '')
     json.dump(data, outfile, indent = 4)
-    print(";\n}", file = outfile)
+    print("\t;\n\n\treturn dims;\n}", file = outfile)
+    outfile.close()
 
+    for module in data:
+        for row in range(16):
+            used = row < module["zegments"]
+            obj = {}
+            obj["rid"] = (module["stack"] * 6 + module["layer"]) * 16 + row
+            obj["use"] = used
+            if (used):
+                obj["stk"] = module["stack"]
+                obj["lyr"] = module["layer"]
+                obj["row"] = row
+                obj["rows"] = module["zegments"]
+                obj["p0"] = {
+                    "y": module["minR"],
+                    "z": round(module["minZ"] + row * module["zsize"], 2)
+                }
+                obj["p1"] = {
+                    "y": module["maxR"],
+                    "z": round(module["minZ"] + (row + 1) * module["zsize"], 2)
+                }
+            padrows.append(obj)
+
+    padrows.sort(key = lambda x: x["rid"])
+
+    outfile = open("jsroot/components/padrow-dimensions.js", "w")
+    print("function getPadrowDimensions() {\nconst dims = ", file = outfile, end = '')
+    json.dump(padrows, outfile, indent = 4)
+    print(";\n\n\treturn dims;\n}", file = outfile)
     outfile.close()
 
 if __name__ == "__main__":
